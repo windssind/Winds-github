a93769811872a0a5e55955a5b7030365a4ad4a04
windssind <118374779+windssind@users.noreply.github.com>
2022-11-16 23:35:15 +0800

    Add files via upload

---
+++ Quciksort.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+void Quicksort(int arr[], int low, int high);
+int main()
+{
+    int arr[5] = { 7,9,4,3,2};
+    Quicksort(arr, 0, 4);
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d", arr[i]);
+    }
+
+}
+void Quicksort(int arr[], int low, int high)
+{
+    if (low >= high)return;
+    int mid = (low + high) / 2;
+    int i = low;
+    int j = high;
+    int temp = 0;
+    int insert = arr[low];
+    while (i < j)
+    {
+        while (i < j && arr[j]>=insert) {
+            j--;
+
+        }
+        while (i < j && arr[i] <= insert) {
+            i++;
+        }
+
+        
+
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+
+
+        arr[low] = arr[i];
+        arr[i] = insert;
+        //只需要将目标的放在找到的位置就行了(在这个算法中,第一个元素必定不会被换走)
+        if(i>low)Quicksort(arr, low, i-1);
+        if(i<high)Quicksort(arr, i + 1, high);
+    }
